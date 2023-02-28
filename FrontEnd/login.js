3d006d5fd3b58532c912c6f9e2e5c0d211f59f31
Petoux <kevin.mail.pro@gmail.com>
2023-02-28 14:46:43 +0100

    Connexion admin terminer

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,9 +1,67 @@
 /** 
  * Gère l'affichage de la page Login et les paramètres de connexion
  */
+const form = document.querySelector('.login-form');
 
-fetch("http://localhost:5678/api/users/login")
-    .then( data => data.json())
-    .then( jsonLogin => {
-        console.log(jsonLogin)  
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+const response = await fetch("http://localhost:5678/api/users/login", {
+    method: 'post',
+    headers: { 'content-Type': 'application/json'
+},
+    body: JSON.stringify({ email, password })
+});
+
+if (response.ok) {
+    const data = await response.json();
+
+    localStorage.setItem('token', data.token);
+
+    window.location.href = 'index.html'
+}else {
+    alert("Identifiant ou mot de passe incorrect");
+  }
+})
+.catch(error => {
+    console.error('erreur:', error);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let loginForm = document.getElementById('login-form');
+
+    loginForm.addEventListener('submit', function(e) {
+
+        let adminMail = "sophie.bluel@test.tld";
+        let adminPassword = "S0phie";
+
+        if(adminMail.value != adminMail) {
+            alert((reponse.message = "Vous ne passerez pas"))
+
+        }else if (adminPassword.value != adminPassword) {
+            alert((reponse.message = "Vous ne passerez pas"))
+        }else {
+            alert('Formulaire envoyer et correct');
+        }
     })
+}); */
+
+
+
+
+//body: JSON.stringify({  email: `${email.value}`, password: `${password.value}`, }); });
