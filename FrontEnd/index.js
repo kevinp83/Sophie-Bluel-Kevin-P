e6a5af4c805bbc0841b7389154cc55bb02358484
Petoux <kevin.mail.pro@gmail.com>
2023-03-08 16:40:30 +0100

    Fonction delete ok, modale 2 50%

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -2,186 +2,222 @@
 const urlAPI = "http://localhost:5678/api/works"
 // Appel a l'API via la const urlAPI
 fetch(urlAPI)
-    .then(function(response){
-    if (response.ok){
-        return response.json(); 
-        //recuperation des données au format json pour pouvoir s'en servir dans la suite du code
+  .then(function (response) {
+    if (response.ok) {
+      return response.json();
+      //recuperation des données au format json pour pouvoir s'en servir dans la suite du code
     }
-})
-.then(function(values) {
+  })
+  .then(function (values) {
     // Affichage des données récuperer dans l'API 
     console.log(values);
 
-    values.forEach( value => {
-// Création d'élément HTML à partir des valeurs JSON récuperer depuis l'API 
-        let gallery = document.querySelector(".gallery");
-        let figure = document.createElement("figure");
-        let img = document.createElement("img");
-        let figcaption = document.createElement("figcaption");
-        let categoryId = document.createElement("p");
+    values.forEach(value => {
+      // Création d'élément HTML à partir des valeurs JSON récuperer depuis l'API 
+      const gallery = document.querySelector(".gallery");
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      const categoryId = document.createElement("p");
+      
+      img.setAttribute("src", value.imageUrl);
+      figcaption.setAttribute("alt", value.title);
+      img.setAttribute("crossorigin", "anonymous");
+      categoryId.setAttribute("src", value.categoryId);
+      categoryId.setAttribute("crossorigin", "anonymous");
+      figcaption.innerHTML = value.title;
+
+      figure.append(img, figcaption);
+      gallery.append(figure);
+
+    })
+    // Création d'une fonction pour filtrer les objets en fonction du bouton de catégorie cliquer sur le site
+    function filterObjets(values, categoryId) {
+
+      let filteredValues;
+
+      // Création de l'option "par défaut" pour dire que SI aucune catégorie n'est choisi (en cliquant sur le bouton 'tous'
+      // alors tout les éléments apparraitront à l'écran)
+      if (categoryId.length === 0) {
+        filteredValues = values;
+
+      } else { //Sinon, on filtrera les objets pour afficher seulement ceux dont la catégorie est mentionner au click boutton
+        filteredValues = values.filter(value => categoryId.includes(value.categoryId));
+      }
+
+      // On vide les éléments HTML présent dans Gallery
+      gallery = document.querySelector(".gallery");
+      gallery.innerHTML = "";
+
+      // Itération sur les objets filtrés pour créer les éléments HTML ci-joints
+      filteredValues.forEach(value => {
+
+        figure = document.createElement("figure");
+        img = document.createElement("img");
+        figcaption = document.createElement("figcaption");
 
         img.setAttribute("src", value.imageUrl);
         figcaption.setAttribute("alt", value.title);
         img.setAttribute("crossorigin", "anonymous");
-        categoryId.setAttribute("src", value.categoryId);
-        categoryId.setAttribute("crossorigin", "anonymous");
+
         figcaption.innerHTML = value.title;
 
-        figure.append(img, figcaption);
         gallery.append(figure);
-    })
-// Création d'une fonction pour filtrer les objets en fonction du bouton de catégorie cliquer sur le site
-    function filterObjets(values, categoryId) {
+        figure.append(img, figcaption);
 
-                let filteredValues;
-
-// Création de l'option "par défaut" pour dire que SI aucune catégorie n'est choisi (en cliquant sur le bouton 'tous'
-// alors tout les éléments apparraitront à l'écran)
-                if (categoryId.length === 0) {
-                    filteredValues = values;
-
-                }else{ //Sinon, on filtrera les objets pour afficher seulement ceux dont la catégorie est mentionner au click boutton
-                    filteredValues = values.filter(value => categoryId.includes(value.categoryId));
-                }
-
-                // On vide les éléments HTML présent dans Gallery
-                let gallery = document.querySelector(".gallery");
-                gallery.innerHTML = "";
-
-// Itération sur les objets filtrés pour créer les éléments HTML ci-joints
-                filteredValues.forEach( value => {
-
-                let figure = document.createElement("figure");
-                let img = document.createElement("img");
-                let figcaption = document.createElement("figcaption");
-
-                img.setAttribute("src", value.imageUrl);
-                figcaption.setAttribute("alt", value.title);
-                img.setAttribute("crossorigin", "anonymous");
-
-                figcaption.innerHTML = value.title;
-
-                figure.append(img, figcaption);
-                gallery.append(figure);
-            
-        });
+      });
     } // Création d'événements "au click" sur différent bouton pour filtrer les éléments selon leur catégorie (bouton) séléctionné
-        const noFilter = document.querySelector(".filter-no");
-        noFilter.addEventListener("click", function() {
-            filterObjets(values, []);
-        });
+    const noFilter = document.querySelector(".filter-no");
+    noFilter.addEventListener("click", function () {
+      filterObjets(values, []);
+    });
 
-        const boutonObjets1 = document.querySelector(".filter-objets");
-        boutonObjets1.addEventListener("click", function() {
-            filterObjets(values, [1]);
-        });
+    const boutonObjets1 = document.querySelector(".filter-objets");
+    boutonObjets1.addEventListener("click", function () {
+      filterObjets(values, [1]);
+    });
 
-        const boutonObjets2 = document.querySelector(".filter-appartements");
-        boutonObjets2.addEventListener("click", function() {
-            filterObjets(values, [2]);
-        });
+    const boutonObjets2 = document.querySelector(".filter-appartements");
+    boutonObjets2.addEventListener("click", function () {
+      filterObjets(values, [2]);
+    });
 
-        const boutonObjets3 = document.querySelector(".filter-hotel");
-        boutonObjets3.addEventListener("click", function() {
-            filterObjets(values, [3]);
-        });
+    const boutonObjets3 = document.querySelector(".filter-hotel");
+    boutonObjets3.addEventListener("click", function () {
+      filterObjets(values, [3]);
+    });
 
-// Partie concernant la modal de modification de la gallerie
-let modal = null 
-// Vérification de la rpésence du token dans session Storage, pour pouvoir générer 
-// l'affichage de la modale et des différent filtre admin
-const token = sessionStorage.getItem('token');
+    let modal = null;
 
-if (token) {
-    document.querySelectorAll('.js-modal').forEach(a => {
-        a.style.display = 'inline-block';
+    let token = sessionStorage.getItem('token');
 
-// Suppression des boutons filtre si token présent dans session storage
-    document.querySelector('.filter').style.display = 'none';
+    if (token) {
+      const modalLinks = document.querySelectorAll('.js-modal');
 
-// Ajout de l'élément "mode édition" si présence du Token
-const headerAdmin = document.querySelector('.header-admin-edition')
-    headerAdmin.style.display = null;
-    headerAdmin.removeAttribute('aria-hidden');
+      modalLinks.forEach((link) => {
+        link.style.display = 'flex';
 
-// Ajout de l'élèment "modifier" en dessous de l'image de sophie bluel
-    let imgBluel = document.querySelector(".img-bluel");
-    let figcaption = document.createElement("figcaption");
+        const filterButton = document.querySelector('.filter');
+        filterButton.style.display = 'none';
 
-    figcaption.setAttribute("crossorigin", "anonymous");
-    imgBluel.append(figcaption);
-    figcaption.innerHTML = "modifier"
+        const headerAdmin = document.querySelector('.header-admin-edition');
+        headerAdmin.style.display = null;
+        headerAdmin.removeAttribute('aria-hidden');
 
-// 
+        const openModal = (e) => {
+          e.preventDefault();
+          closeModal();
 
+          const target = document.querySelector(e.target.getAttribute('href'));
 
-    const openModal = function (e) {
+          target.style.display = 'block';
+          target.removeAttribute('aria-hidden');
+          target.setAttribute('aria-modal', 'true');
+          modal = target;
+
+          const modalGallery = modal.querySelector('.modal-gallery');
+          modalGallery.innerHTML = '';
+
+          values.forEach((value) => {
+            figure = document.createElement('figure');
+            img = document.createElement('img');
+            figcaption = document.createElement('figcaption');
+            categoryId = document.createElement('p');
+            const delete1Work = document.createElement("i");
+
+            img.setAttribute('src', value.imageUrl);
+            figcaption.setAttribute('alt', value.title);
+            figcaption.textContent = 'Editer';
+            categoryId.setAttribute('src', value.categoryId);
+            figure.append(img, figcaption, delete1Work);
+            modalGallery.append(figure);
+            delete1Work.classList.add("fa-solid", "fa-trash-can");
+
+            
+            function deleteWork(id) {
+              console.log(id);
+            
+              fetch(`http://localhost:5678/api/works/${id}`, {
+                method: "DELETE",
+                headers: { Authorization : `Bearer ${token}`},
+              })
+              .then((response) => response.json())
+              .then((json) => console.log(json));
+            };
+
+            delete1Work.addEventListener("click", function () {
+              deleteWork(value.id);
+            });
+
+            });
+
+          const openModal2 = (e) => {
+            e.preventDefault();
+            closeModal();
+
+            const target2 = document.querySelector('#modal2');
+
+            target2.style.display = 'block';
+            target2.removeAttribute('aria-hidden');
+            target2.setAttribute('aria-modal', 'true');
+            modal = target2;
+          };
+
+          const modalBtnAddPhoto = modal.querySelector('.modal-btn-add-photo');
+          modalBtnAddPhoto.addEventListener('click', openModal2);
+
+          modal.addEventListener('click', closeModal);
+          modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
+          modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+        };
+
+        link.addEventListener('click', openModal);
+      });
+    } else {
+      const modalLinks = document.querySelectorAll('.js-modal');
+
+      modalLinks.forEach((link) => {
+        link.style.display = 'none';
+      });
+    }
+
+    const closeModal = (e) => {
+      if (modal === null) return;
+
+      if (e) {
         e.preventDefault();
+      }
 
-    const target = document.querySelector(e.target.getAttribute('href'));
-// Affichage de la modale en modifiant le display, et  les attribut aria-hidden et aria-modal
-        target.style.display = 'block';
-        target.removeAttribute('aria-hidden');
-        target.setAttribute('aria-modal', 'true');
-        modal = target;
-// On ajoute les écouteurs d'événements de fermeture de modal
-        modal.addEventListener('click', closeModal);
-        modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
-        modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+      modal.style.display = 'none';
+      modal.setAttribute('aria-hidden', 'true');
+      modal.removeAttribute('aria-modal');
+      modal = null;
+    };
 
-// On vide la modale avant chaque ouverture, pour éviter le chargement des images en double-triple etc
-    document.querySelector(".modal-gallery").innerHTML = "";
+    const stopPropagation = (e) => {
+      e.stopPropagation();
+    };
 
-    values.forEach( value => {
-        // Création d'élément HTML à partir des valeurs JSON récuperer depuis l'API 
-                let gallery = document.querySelector(".modal-gallery");
-                let figure = document.createElement("figure");
-                let img = document.createElement("img");
-                let figcaption = document.createElement("figcaption");
-                let categoryId = document.createElement("p");
-        
-                img.setAttribute("src", value.imageUrl);
-                figcaption.setAttribute("alt", value.title);
-                img.setAttribute("crossorigin", "anonymous");
-                categoryId.setAttribute("src", value.categoryId);
-                categoryId.setAttribute("crossorigin", "anonymous");
-                figcaption.innerHTML = "Editer";
-
-                figure.append(img, figcaption);
-                gallery.append(figure);
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' || e.key === 'Esc') {
+        closeModal(e);
+      }
     });
-};
-        a.addEventListener('click', openModal);
+
+    /* Afficher l'image en miniature, ne fonctionne pas */
+    const inputFile = document.querySelector("#file-input");
+    const modalImage = document.querySelector(".modal-add-img");
+
+    inputFile.addEventListener("change", function () {
+      if (img) {
+        modalImage.removeChild(img);
+      }
+
+      img = document.createElement("img");
+      img.src = window.URL.createObjectURL(this.files[0]);
+      img.onload = function () {
+        URL.revokeObjectURL(this.src);
+      };
+      modalImage.appendChild(img);
     });
-}else{
-    document.querySelectorAll('.js-modal').forEach(a => {
-        a.style.display = 'none';
-    });
-}
-
-const closeModal = function (e) {
-    if (modal === null) return
-    e.preventDefault()
-    modal.style.display = "none"
-    modal.setAttribute('aria-hidden', 'true')
-    modal.removeAttribute('aria-modal')
-    modal.removeEventListener('click', closeModal)
-    modal.querySelector('.js-close-modal').removeEventListener('click', closeModal)
-    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation)
-    modal = null
-    }
-
-const stopPropagation = function (e) {
-    e.stopPropagation()
-}
-
-document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openModal)
-})
-
- window.addEventListener('keydown', function (e) {
-    if (e.key === "Escape" || e.key === "Esc") {
-        closeModal(e)
-    }
- });
-});
+  })
--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -9,36 +9,37 @@ const form = document.querySelector('.login-form');
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
-// Récupération des champs du formulaire nécessaire pour la requête API
-let adminInfo = {
-     email: form.elements.email.value,
-     password: form.elements.password.value
-};
+    // Récupération des champs du formulaire nécessaire pour la requête API
+    let adminInfo = {
+        email: form.elements.email.value,
+        password: form.elements.password.value
+    };
 
-// Requête POST asynchrone à l'API Swagger avec les informations nécessaire pour l'identification
+    // Requête POST asynchrone à l'API Swagger avec les informations nécessaire pour l'identification
     const response = await fetch("http://localhost:5678/api/users/login", {
         method: 'post',
-        headers: { 'content-Type': 'application/json'
-},
+        headers: {
+            'content-Type': 'application/json'
+        },
         body: JSON.stringify(adminInfo)
-});
-// Ce qu'il se passe si la requête envoyer a l'API est du statut 200 : D'abord récuperer les données reçues par l'API au format JSON
+    });
+    // Ce qu'il se passe si la requête envoyer a l'API est du statut 200 : D'abord récuperer les données reçues par l'API au format JSON
     if (response.ok) {
         const data = await response.json();
 
-// Enregistrement du Token dans le local storage afin de pouvoir s'en servir pour la suite du Projet
+        // Enregistrement du Token dans le local storage afin de pouvoir s'en servir pour la suite du Projet
         sessionStorage.setItem('token', data.token);
 
-// Rediriger l'utilisateur sur la page d'accueil
+        // Rediriger l'utilisateur sur la page d'accueil
         window.location.href = 'index.html'
 
-    }else{ // Si requête négatif (statut autre que 200), envoyer un message d'alerte sur l'écran
+    } else { // Si requête négatif (statut autre que 200), envoyer un message d'alerte sur l'écran
         alert("Identifiant ou mot de passe incorrect");
     }
 })
     .catch(error => { // Code pour les autres problèmes que l'on pourrait rencontrer 
         console.error('erreur:', error);
-});
+    });
 
 
 
