d0e39e9a68c039ba26d5389180504de2f5e42b2d
Petoux <kevin.mail.pro@gmail.com>
2023-03-09 16:45:19 +0100

    Modal 2 70%, fermeture ok

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,72 +1,20 @@
 // Ici je met l'url de l'API sous const pour pouvoir l'appeler plus facilement par la suite
-const urlAPI = "http://localhost:5678/api/works"
+const urlAPI = "http://localhost:5678/api/works";
 // Appel a l'API via la const urlAPI
 fetch(urlAPI)
   .then(function (response) {
     if (response.ok) {
-      return response.json();
-      //recuperation des données au format json pour pouvoir s'en servir dans la suite du code
+      return response.json();    //recuperation des données au format json pour pouvoir s'en servir dans la suite du code
     }
   })
   .then(function (values) {
-    // Affichage des données récuperer dans l'API 
     console.log(values);
 
-    values.forEach(value => {
-      // Création d'élément HTML à partir des valeurs JSON récuperer depuis l'API 
-      const gallery = document.querySelector(".gallery");
-      const figure = document.createElement("figure");
-      const img = document.createElement("img");
-      const figcaption = document.createElement("figcaption");
-      const categoryId = document.createElement("p");
-      
-      img.setAttribute("src", value.imageUrl);
-      figcaption.setAttribute("alt", value.title);
-      img.setAttribute("crossorigin", "anonymous");
-      categoryId.setAttribute("src", value.categoryId);
-      categoryId.setAttribute("crossorigin", "anonymous");
-      figcaption.innerHTML = value.title;
+    values.forEach((value) => {
+      displayOne(value);
+    });
 
-      figure.append(img, figcaption);
-      gallery.append(figure);
-
-    })
-    // Création d'une fonction pour filtrer les objets en fonction du bouton de catégorie cliquer sur le site
-    function filterObjets(values, categoryId) {
-
-      let filteredValues;
-
-      // Création de l'option "par défaut" pour dire que SI aucune catégorie n'est choisi (en cliquant sur le bouton 'tous'
-      // alors tout les éléments apparraitront à l'écran)
-      if (categoryId.length === 0) {
-        filteredValues = values;
-
-      } else { //Sinon, on filtrera les objets pour afficher seulement ceux dont la catégorie est mentionner au click boutton
-        filteredValues = values.filter(value => categoryId.includes(value.categoryId));
-      }
-
-      // On vide les éléments HTML présent dans Gallery
-      gallery = document.querySelector(".gallery");
-      gallery.innerHTML = "";
-
-      // Itération sur les objets filtrés pour créer les éléments HTML ci-joints
-      filteredValues.forEach(value => {
-
-        figure = document.createElement("figure");
-        img = document.createElement("img");
-        figcaption = document.createElement("figcaption");
-
-        img.setAttribute("src", value.imageUrl);
-        figcaption.setAttribute("alt", value.title);
-        img.setAttribute("crossorigin", "anonymous");
-
-        figcaption.innerHTML = value.title;
-
-        gallery.append(figure);
-        figure.append(img, figcaption);
-
-      });
-    } // Création d'événements "au click" sur différent bouton pour filtrer les éléments selon leur catégorie (bouton) séléctionné
+    // Création d'événements "au click" sur différent bouton pour filtrer les éléments selon leur catégorie (bouton) séléctionné
     const noFilter = document.querySelector(".filter-no");
     noFilter.addEventListener("click", function () {
       filterObjets(values, []);
@@ -89,95 +37,100 @@ fetch(urlAPI)
 
     let modal = null;
 
-    let token = sessionStorage.getItem('token');
+    let token = sessionStorage.getItem("token");
 
-    if (token) {
-      const modalLinks = document.querySelectorAll('.js-modal');
+    if (token) {  // Mise en page de toute les actions possible si le token est présent
+      const modalLinks = document.querySelectorAll(".js-modal");
 
       modalLinks.forEach((link) => {
-        link.style.display = 'flex';
+        link.style.display = "flex";
 
-        const filterButton = document.querySelector('.filter');
-        filterButton.style.display = 'none';
+        const filterButton = document.querySelector(".filter");
+        filterButton.style.display = "none";
 
-        const headerAdmin = document.querySelector('.header-admin-edition');
+        const headerAdmin = document.querySelector(".header-admin-edition");
         headerAdmin.style.display = null;
-        headerAdmin.removeAttribute('aria-hidden');
+        headerAdmin.removeAttribute("aria-hidden");
 
         const openModal = (e) => {
           e.preventDefault();
           closeModal();
 
-          const target = document.querySelector(e.target.getAttribute('href'));
+          const target = document.querySelector(e.target.getAttribute("href"));
 
-          target.style.display = 'block';
-          target.removeAttribute('aria-hidden');
-          target.setAttribute('aria-modal', 'true');
+          target.style.display = "block";
+          target.removeAttribute("aria-hidden");
+          target.setAttribute("aria-modal", "true");
           modal = target;
 
-          const modalGallery = modal.querySelector('.modal-gallery');
-          modalGallery.innerHTML = '';
+          const modalGallery = modal.querySelector(".modal-gallery");
+          modalGallery.innerHTML = "";
 
           values.forEach((value) => {
-            figure = document.createElement('figure');
-            img = document.createElement('img');
-            figcaption = document.createElement('figcaption');
-            categoryId = document.createElement('p');
+            figure = document.createElement("figure");
+            img = document.createElement("img");
+            figcaption = document.createElement("figcaption");
+            categoryId = document.createElement("p");
             const delete1Work = document.createElement("i");
 
-            img.setAttribute('src', value.imageUrl);
-            figcaption.setAttribute('alt', value.title);
-            figcaption.textContent = 'Editer';
-            categoryId.setAttribute('src', value.categoryId);
+            img.setAttribute("src", value.imageUrl);
+            figcaption.setAttribute("alt", value.title);
+            figcaption.textContent = "Editer";
+            categoryId.setAttribute("src", value.categoryId);
+            delete1Work.classList.add("fa-solid", "fa-trash-can");
             figure.append(img, figcaption, delete1Work);
             modalGallery.append(figure);
-            delete1Work.classList.add("fa-solid", "fa-trash-can");
-
-            
-            function deleteWork(id) {
-              console.log(id);
-            
-              fetch(`http://localhost:5678/api/works/${id}`, {
-                method: "DELETE",
-                headers: { Authorization : `Bearer ${token}`},
-              })
-              .then((response) => response.json())
-              .then((json) => console.log(json));
-            };
 
             delete1Work.addEventListener("click", function () {
-              deleteWork(value.id);
+              deleteWork(value.id, token);
             });
+          });
 
-            });
+          const modalBtnAddPhoto = modal.querySelector(".modal-btn-add-photo");
+          modalBtnAddPhoto.addEventListener("click", openModal2);
 
-          const openModal2 = (e) => {
-            e.preventDefault();
-            closeModal();
-
-            const target2 = document.querySelector('#modal2');
-
-            target2.style.display = 'block';
-            target2.removeAttribute('aria-hidden');
-            target2.setAttribute('aria-modal', 'true');
-            modal = target2;
-          };
-
-          const modalBtnAddPhoto = modal.querySelector('.modal-btn-add-photo');
-          modalBtnAddPhoto.addEventListener('click', openModal2);
-
-          modal.addEventListener('click', closeModal);
-          modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
-          modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+          modal.addEventListener("click", closeModal);
+          modal
+            .querySelector(".js-close-modal")
+            .addEventListener("click", closeModal);
+          modal
+            .querySelector(".js-modal-stop")
+            .addEventListener("click", stopPropagation);
         };
 
-        link.addEventListener('click', openModal);
+        link.addEventListener("click", openModal);
+
+        const openModal2 = (e) => {
+          e.preventDefault();
+          closeModal();
+
+          const target2 = document.querySelector("#modal2");
+
+          target2.style.display = "block";
+          target2.removeAttribute("aria-hidden");
+          target2.setAttribute("aria-modal", "true");
+          modal = target2;
+
+          modal
+            .querySelector(".js-close-modal")
+            .addEventListener("click", closeModal);
+          modal
+            .querySelector(".js-modal-stop")
+            .addEventListener("click", stopPropagation);
+          modal
+            .querySelector(".back")
+            .addEventListener("click", function() {
+              closeModal();
+              openModal();
+            });
+        };
+
       });
     } else {
-      const modalLinks = document.querySelectorAll('.js-modal');
+      const modalLinks = document.querySelectorAll(".js-modal");
 
       modalLinks.forEach((link) => {
-        link.style.display = 'none';
+        link.style.display = "none";
       });
     }
 
@@ -188,9 +141,9 @@ fetch(urlAPI)
         e.preventDefault();
       }
 
-      modal.style.display = 'none';
-      modal.setAttribute('aria-hidden', 'true');
-      modal.removeAttribute('aria-modal');
+      modal.style.display = "none";
+      modal.setAttribute("aria-hidden", "true");
+      modal.removeAttribute("aria-modal");
       modal = null;
     };
 
@@ -198,8 +151,8 @@ fetch(urlAPI)
       e.stopPropagation();
     };
 
-    window.addEventListener('keydown', (e) => {
-      if (e.key === 'Escape' || e.key === 'Esc') {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape" || e.key === "Esc") {
         closeModal(e);
       }
     });
@@ -221,3 +174,57 @@ fetch(urlAPI)
       modalImage.appendChild(img);
     });
   })
+  .catch(function (err) {
+    console.log("erreur");
+  });
+
+function displayOne(value) {
+  // Création d'élément HTML à partir des valeurs JSON récuperer depuis l'API
+  let gallery = document.querySelector(".gallery");
+  let figure = document.createElement("figure");
+  let img = document.createElement("img");
+  let figcaption = document.createElement("figcaption");
+
+  img.setAttribute("src", value.imageUrl);
+  figcaption.setAttribute("alt", value.title);
+  img.setAttribute("crossorigin", "anonymous");
+
+  figcaption.innerHTML = value.title;
+
+  figure.append(img, figcaption);
+  gallery.append(figure);
+};
+
+// Création d'une fonction pour filtrer les objets en fonction du bouton de catégorie cliquer sur le site
+function filterObjets(values, categoryId) {
+  let filteredValues;
+
+  // Création de l'option "par défaut" pour dire que SI aucune catégorie n'est choisi (en cliquant sur le bouton 'tous'
+  // alors tout les éléments apparraitront à l'écran)
+  if (categoryId.length === 0) {
+    filteredValues = values;
+  } else {
+    //Sinon, on filtrera les objets pour afficher seulement ceux dont la catégorie est mentionner au click boutton
+    filteredValues = values.filter((value) =>
+      categoryId.includes(value.categoryId)
+    );
+  }
+  // On vide les éléments HTML présent dans Gallery
+  document.querySelector(".gallery").innerHTML = "";
+
+  // Itération sur les objets filtrés pour créer les éléments HTML ci-joints
+  filteredValues.forEach((value) => {
+    displayOne(value);
+  });
+};
+
+function deleteWork(id, token) {
+  console.log(id);
+
+  fetch(`http://localhost:5678/api/works/${id}`, {
+    method: "DELETE",
+    headers: { Authorization: `Bearer ${token}` },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
